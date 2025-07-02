9ed39e92c3b52ad4ef6bc66670dbe04a694e20b2
Carl John B. Villavito <carljohnvillavito2023@gmail.com>
2025-07-02 19:20:55 +0800

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/**/*.{html,js}"], // Scan HTML and JS files in the public folder
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
